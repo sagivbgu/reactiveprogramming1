5dab989e06729fe97adc10adc1f29421e3f56c89
Matanel <matan848@gmail.com>
2019-04-09 17:45:08 +0300

    generate_sentence

--- task_3.py
+++ task_3.py
@@ -1,3 +1,3 @@
 def generate_sentence(subjects, verbs, objects):
-    for subject, verb, object in zip(subjects, verbs, objects):
-        yield " ".join((subject, verb, object))
+    for subject, verb, obj in zip(subjects, verbs, objects):
+        yield " ".join((subject, verb, obj))
