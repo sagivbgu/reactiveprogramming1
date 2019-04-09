9f62728d4410caa8233e4b3d6d850915692206c8
Matanel <matan848@gmail.com>
2019-04-09 17:28:58 +0300

    morse code

--- task_2.py
+++ task_2.py
@@ -18,3 +18,25 @@ def encrypt_caesar_cipher(plaintext):
 
 def decrypt_caesar_cipher(ciphertext):
     return _caesar_cipher(ciphertext, False)
+
+
+def _morse_code_translation(text, encrypt):
+    translation_table = {"A": ".-", "B": "-...", "C": "-.-.", "D": "-..", "E": ".", "F": "..-.", "G": "--.",
+                         "H": "....", "I": "..", "J": ".---", "K": "-.-", "L": ".-..", "M": "--", "N": "-.",
+                         "O": "---", "P": ".--.", "Q": "--.-", "R": ".-.", "S": "...", "T": "-", "U": "..-",
+                         "V": "...-", "W": ".--", "X": "-..-", "Y": "-.--", "Z": "--..",
+                         "1": ".----", "2": "..---", "3": "...--", "4": "....-", "5": ".....", "6": "-....",
+                         "7": "--...", "8": "---..", "9": "----.", "0": "-----"}
+    if not encrypt:
+        translation_table = {v: k for k, v in translation_table.items()}
+
+    # TODO: Not sure how to join
+    return "".join((translation_table[x] if x in translation_table else x for x in text))
+
+
+def encrypt_morse_code(plaintext):
+    return _morse_code_translation(plaintext, True)
+
+
+def decrypt_morse_code(ciphertext):
+    return _morse_code_translation(ciphertext, False)
