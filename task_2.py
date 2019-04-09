0feee1ccfe995358b06c647b2854341bab56137e
sagiv <sagivsc@post.bgu.ac.il>
2019-04-09 18:32:26 +0300

    vigenere_cipher

--- task_2.py
+++ task_2.py
@@ -1,20 +1,34 @@
 from string import ascii_uppercase
-
-
-def _caesar_cipher(text, encrypt):
-    shift = 3
-    if not encrypt:
-        shift = -shift
-
-    def shift_letter(letter):
-        return ascii_uppercase[(ascii_uppercase.find(letter) + shift) % len(ascii_uppercase)]
-
-    return "".join((shift_letter(letter) if letter.isalpha() else letter for letter in text))
+from itertools import cycle
 
 
 def encrypt_caesar_cipher(plaintext):
-    return _caesar_cipher(plaintext, True)
+    return _caesar_cipher(plaintext, 3)
 
 
 def decrypt_caesar_cipher(ciphertext):
-    return _caesar_cipher(ciphertext, False)
+    return _caesar_cipher(ciphertext, -3)
+
+
+def encrypt_vigenere_cipher(plaintext, keyword):
+    return _vigenere_cipher(plaintext, keyword, ascii_uppercase.find)
+
+
+def decrypt_vigenere_cipher(ciphertext, keyword):
+    return _vigenere_cipher(ciphertext, keyword, lambda letter: - ascii_uppercase.find(letter))
+
+
+def _caesar_cipher(text, shift):
+    return "".join((_shift_letter(letter, shift) if letter.isalpha() else letter for letter in text))
+
+
+def _vigenere_cipher(text, keyword, shift_func):
+    if not keyword:
+        keyword = "A"
+    return "".join((_shift_letter(letter, shift_func(shift_letter)) if letter.isalpha() else letter
+                    for letter, shift_letter in zip(text, cycle(keyword))
+                    ))
+
+
+def _shift_letter(letter, shift):
+    return ascii_uppercase[(ascii_uppercase.find(letter) + shift) % len(ascii_uppercase)]
--- task_2_tests.py
+++ task_2_tests.py
@@ -11,3 +11,21 @@ def test_decrypt_caesar_cipher():
     assert decrypt_caesar_cipher("") == ""
     assert decrypt_caesar_cipher("SBWKRQ") == "PYTHON"
     assert decrypt_caesar_cipher("I1UVW S0VW") == "F1RST P0ST"
+
+
+def test_encrypt_vigenere_cipher():
+    assert encrypt_vigenere_cipher("", "") == ""
+    assert encrypt_vigenere_cipher("", "A") == ""
+    assert encrypt_vigenere_cipher("A", "") == "A"
+    assert encrypt_vigenere_cipher("ATTACKATDAWN", "LEMON") == "LXFOPVEFRNHR"
+    assert encrypt_vigenere_cipher("AT", "LEMON") == "LX"
+    assert encrypt_vigenere_cipher("ATT4CKATD9WN.", "LEMON") == "LXF4PVEFR9HR."
+
+
+def test_decrypt_vigenere_cipher():
+    assert decrypt_vigenere_cipher("", "") == ""
+    assert decrypt_vigenere_cipher("", "A") == ""
+    assert decrypt_vigenere_cipher("A", "") == "A"
+    assert decrypt_vigenere_cipher("LXFOPVEFRNHR", "LEMON") == "ATTACKATDAWN"
+    assert decrypt_vigenere_cipher("LX", "LEMON") == "AT"
+    assert decrypt_vigenere_cipher("LXF4PVEFR9HR", "LEMON") == "ATT4CKATD9WN"
