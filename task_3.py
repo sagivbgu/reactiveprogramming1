93cac25ac052c44a0a1aa63d26a1e8b988994529
sagiv <sagivsc@post.bgu.ac.il>
2019-04-20 18:06:16 +0300

    Task 3

--- task_3.py
+++ task_3.py
@@ -1,3 +1,25 @@
+def generate_n_grams(size, words_list):
+    words = words_list.split(" ")
+    for i in range(len(words) - size + 1):
+        yield words[i:i + size]
+
+
 def generate_sentence(subjects, verbs, objects):
     for subject, verb, obj in zip(subjects, verbs, objects):
         yield " ".join((subject, verb, obj))
+
+
+def generate_permutations(some_list):
+    if len(some_list) == 1:
+        yield tuple(some_list[0])
+
+    elif len(some_list) == 2:
+        yield (some_list[0], some_list[1])
+        yield (some_list[1], some_list[0])
+
+    else:
+        for i in range(len(some_list)):
+            new_list = some_list[:]
+            new_list.pop(i)
+            for inner_perm in generate_permutations(new_list):
+                yield (some_list[i],) + inner_perm
--- task_3_tests.py
+++ task_3_tests.py
@@ -1,4 +1,30 @@
+from types import GeneratorType
+
 from task_3 import *
+
+
+def test_generate_n_grams():
+    sentence = "the quick red fox jumps over the lazy brown dog"
+
+    assert type(generate_n_grams(3, sentence)) == GeneratorType
+
+    expected_lists = (
+        ["the", "quick", "red"],
+        ["quick", "red", "fox"],
+        ["red", "fox", "jumps"],
+        ["fox", "jumps", "over"],
+        ["jumps", "over", "the"],
+        ["over", "the", "lazy"],
+        ["the", "lazy", "brown"],
+        ["lazy", "brown", "dog"]
+    )
+
+    assert tuple(generate_n_grams(3, sentence)) == expected_lists
+
+
+def test_generate_n_grams_size_exceeds():
+    sentence = "the quick red fox jumps over the lazy brown dog"
+    assert tuple(generate_n_grams(100, sentence)) == ()
 
 
 def test_generate_sentence():
@@ -6,3 +32,31 @@ def test_generate_sentence():
     expected_sentences = ["I play Basketball", "You love Football"]
     for sentence, expected_sentence in zip(sentences, expected_sentences):
         assert sentence == expected_sentence
+
+
+def test_generate_permutations_example1():
+    some_list = [1, 2]
+    expected_permutations = (
+        (1, 2),
+        (2, 1)
+    )
+
+    assert tuple(generate_permutations(some_list)) == expected_permutations
+
+
+def test_generate_permutations_example2():
+    some_list = [1, 'a', True]
+    expected_permutations = (
+        (1, 'a', True),
+        (1, True, 'a'),
+        ('a', 1, True),
+        ('a', True, 1),
+        (True, 1, 'a'),
+        (True, 'a', 1)
+    )
+
+    assert tuple(generate_permutations(some_list)) == expected_permutations
+
+
+def test_generate_permutations_empty_list():
+    assert tuple(generate_permutations([])) == ()
