197858846ce375700f8afb647d1fbf313ca19c9d
sagiv <sagivsc@post.bgu.ac.il>
2019-04-23 18:00:14 +0300

    Extracted caesar cipher shift to const moved "isalpha" logic into _shift_letter

--- task_2.py
+++ task_2.py
@@ -1,13 +1,15 @@
 from string import ascii_uppercase
 from itertools import cycle
 
+CAESAR_CIPHER_SHIFT = 3
+
 
 def encrypt_caesar_cipher(plaintext):
-    return _caesar_cipher(plaintext, 3)
+    return _caesar_cipher(plaintext, CAESAR_CIPHER_SHIFT)
 
 
 def decrypt_caesar_cipher(ciphertext):
-    return _caesar_cipher(ciphertext, -3)
+    return _caesar_cipher(ciphertext, -CAESAR_CIPHER_SHIFT)
 
 
 def encrypt_vigenere_cipher(plaintext, keyword):
@@ -19,16 +21,18 @@ def decrypt_vigenere_cipher(ciphertext, keyword):
 
 
 def _caesar_cipher(text, shift):
-    return "".join((_shift_letter(letter, shift) if letter.isalpha() else letter for letter in text))
+    return "".join((_shift_letter(letter, shift) for letter in text))
 
 
 def _vigenere_cipher(text, keyword, shift_func):
     if not keyword:
         keyword = "A"
-    return "".join((_shift_letter(letter, shift_func(shift_letter)) if letter.isalpha() else letter
+    return "".join((_shift_letter(letter, shift_func(shift_letter))
                     for letter, shift_letter in zip(text, cycle(keyword))
                     ))
 
 
 def _shift_letter(letter, shift):
+    if not letter.isalpha():
+        return letter
     return ascii_uppercase[(ascii_uppercase.find(letter) + shift) % len(ascii_uppercase)]
