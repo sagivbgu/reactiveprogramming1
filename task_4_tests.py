f35110c407b3b3f1d4582ee8b0488a65636d1f5b
sagiv <sagivsc@post.bgu.ac.il>
2019-05-01 18:26:15 +0300

    Updated tests

--- task_4_tests.py
+++ task_4_tests.py
@@ -1,4 +1,7 @@
 from statistics import mean
+
+import pytest
+
 from task_4 import *
 
 DEFAULT_BOOK = r"recipes/small-db.json"
@@ -6,78 +9,54 @@ TEST_DB_PATH = "recipes/test-db.json"
 LARGE_DATA_DB_PATH = "recipes/20170107-061401-recipeitems.json"
 
 
-def test_search_empty_filters():
-    assert any(search(file_name=TEST_DB_PATH))
+def test_search_no_args():
+    with pytest.raises(ValueError):
+        search(file_name=TEST_DB_PATH)
 
 
 def test_search_no_results():
-    keywords = ["pp"]
-    assert not any(search(file_name=TEST_DB_PATH, keywords=keywords))
-
-    keywords = ["aa", "pp"]
-    assert not any(search(file_name=TEST_DB_PATH, keywords=keywords))
-
-    keywords = ["aa"]
-    ingredients_to_exclude = ["one", "two"]
-    assert not any(
-        search(file_name=TEST_DB_PATH, keywords=keywords, ingredients_to_exclude=ingredients_to_exclude))
-
-    keywords = ["aa", "First"]
-    ingredients_to_include = ["nine"]
-    assert not any(
-        search(file_name=TEST_DB_PATH, keywords=keywords, ingredients_to_include=ingredients_to_include))
+    assert not any(search(file_name=TEST_DB_PATH, "pp"))
+    assert not any(search(file_name=TEST_DB_PATH, "aa", "pp"))
+    assert not any(search(file_name=TEST_DB_PATH, "aa", "-one", "-two"))
+    assert not any(search(file_name=TEST_DB_PATH, "aa", "First", "+nine"))
 
 
 def test_search_sanity():
-    keywords = ["ver", "yummy"]
-    ingredients_to_include = ["two", "2"]
-    output = search(keywords=keywords, ingredients_to_include=ingredients_to_include, file_name=TEST_DB_PATH)
-
+    output = search(file_name=TEST_DB_PATH, "ver", "yummy", "+two", "+2")
     output = list(output)
     assert len(output) == 2
     assert output[0]["name"] == "First name"
     assert output[1]["name"] == "Third name"
 
-    keywords = ["yummy"]
-    ingredients_to_include = ["one", "1"]
-    output = search(file_name=TEST_DB_PATH, keywords=keywords, ingredients_to_include=ingredients_to_include)
-
+    output = search(file_name=TEST_DB_PATH, "yummy", "+one", "+1")
     output = list(output)
     assert len(output) == 1
     assert output[0]["name"] == "First name"
 
-    keywords = ["yummy"]
-    ingredients_to_include = ["two", "2"]
-    ingredients_to_exclude = ["one", "1"]
-    output = search(file_name=TEST_DB_PATH, keywords=keywords, ingredients_to_include=ingredients_to_include,
-                    ingredients_to_exclude=ingredients_to_exclude)
-
+    output = search(file_name=TEST_DB_PATH, "yummy", "+two", "+2", "-one", "-1")
     output = list(output)
     assert len(output) == 1
     assert output[0]["name"] == "Third name"
 
-    keywords = ["yummy"]
-    ingredients_to_exclude = ["one"]
-    output = search(file_name=TEST_DB_PATH, keywords=keywords, ingredients_to_exclude=ingredients_to_exclude)
-
+    output = search(file_name=TEST_DB_PATH, "yummy" "-one")
     output = list(output)
     assert len(output) == 1
     assert output[0]["name"] == "Third name"
 
 
 def test_search_large_data():
-    output = search(file_name=LARGE_DATA_DB_PATH)
+    output = search(file_name=LARGE_DATA_DB_PATH, "")
     assert next(output)
 
 
 def test_search_large_data2():
-    output = search(keywords=["Dinner"], file_name=LARGE_DATA_DB_PATH)
+    output = search(file_name=LARGE_DATA_DB_PATH, "Dinner")
     assert next(output)
 
 
 def test_two_searches_in_parallel():
-    output1 = search(file_name=TEST_DB_PATH)
-    output2 = search(file_name=TEST_DB_PATH)
+    output1 = search(file_name=TEST_DB_PATH, "")
+    output2 = search(file_name=TEST_DB_PATH, "")
     next(output1)
     next(output2)
 
@@ -87,23 +66,15 @@ def test_two_searches_in_parallel():
 
 # function can be: average, count, max, min, sum
 def test_statistics_average():
-    assert statistics(mean, "recipeYield", _get_test_recipe_book()) == (8 + 8 + 12) / 3
+    assert statistics(aggregate_function=mean, data=_get_test_recipe_book(), property="recipeYield") == (8 + 8 + 12) / 3
 
 
 def test_statistics_max():
-    assert statistics(max, "recipeYield", _get_test_recipe_book()) == 12
-
-
-def test_statistics_min():
-    assert statistics(min, "recipeYield", _get_test_recipe_book()) == 8
-
-
-def test_statistics_sum():
-    assert statistics(sum, "recipeYield", _get_test_recipe_book()) == 8 + 8 + 12
+    assert statistics(aggregate_function=max, data=_get_test_recipe_book(), property="recipeYield") == 12
 
 
 def test_statistics_count():
-    raise NotImplementedError
+    assert statistics(len, data=_get_test_recipe_book()) == 19
 
 
 def _get_test_recipe_book():
